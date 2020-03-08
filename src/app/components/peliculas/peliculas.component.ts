1466d3ee18c91a93accd29889966061f72beca6c
Alex <omareyes-96@hotmail.com>
2020-03-08 12:45:10 -0500

    Directivas ngStyle ngClass y ngTemplate

--- src/app/components/peliculas/peliculas.component.ts
+++ src/app/components/peliculas/peliculas.component.ts
@@ -7,15 +7,23 @@ import { Component, OnInit,DoCheck,OnDestroy } from '@angular/core';
 })
 export class PeliculasComponent implements OnInit, DoCheck, OnDestroy {
   public titulo:string;
+  public peliculas:Array<any>; // la variable pelicula puedo utilizaral en la vista
   constructor() {
                    //nose debe meter logica dentro del constructor
                   // para asignar valores a las proiedades de las clases 
     this.titulo='Componente Peliculas';
-    console.log('constructor lanzado');
+    //console.log('constructor lanzado');
+    this.peliculas=[
+      {year:2020,titulo:"Camino hacia el terror",image:"https://pelisplus.me/cover/movie/camino-hacia-el-terror.png"}, // se pone , porque es un array
+      {year:2017,titulo:"Mister Bean", image:"https://es.web.img3.acsta.net/r_1280_720/pictures/15/11/05/10/15/561033.png"},
+      {year:2019,titulo:"Dragon Ball",image:"https://media.metrolatam.com/2019/06/06/1501972698dragon-e1caecd2afcb1e6afd0a7c6881c49428-600x400.jpg"},
+      {year:2015,titulo:"Rampage",image:"https://www.beon.com/wp-content/uploads/2018/08/Rampage.png"}
+    ];
    }
 
   ngOnInit() { //aquie si se puede meter funcionalidad
     console.log('On init iniciado');
+    console.log(this.peliculas);
   }
   ngDoCheck(){ //docheck se ejecuta cuando se genera un cambio en el componente
     console.log('Docheck lanzado');
